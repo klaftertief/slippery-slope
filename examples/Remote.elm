b635a978c3f38a9828fb2909f04ebdf14e48430f
Jonas Coch <jonas@iosphere.de>
2017-04-18 08:41:59 +0200

    Better SVG styling support

--- examples/Remote.elm
+++ examples/Remote.elm
@@ -16,12 +16,29 @@ import SlippyMap.Geo.Transform as Transform exposing (Transform)
 import SlippyMap.LowLevel as LowLevel
 import SlippyMap.SimpleGeoJson as SimpleGeoJson
 import Svg exposing (Svg)
+import Svg.Attributes
 
 
 type alias Model =
     { transform : Transform
-    , tiles : Dict Tile.Comparable (WebData (List ( String, GeoJson )))
+    , tiles : Dict Tile.Comparable (WebData (List Feature))
     , drag : Maybe Drag
+    }
+
+
+type alias Feature =
+    { properties : Maybe FeatureProperties
+    , geometry : GeoJson.Geometry
+    }
+
+
+type alias FeatureProperties =
+    { layerName : String
+    , name : Maybe String
+    , kind : String
+    , minZoom : Float
+    , sortRank : Int
+    , labelPlacement : Bool
     }
 
 
@@ -82,10 +99,10 @@ gestureLayer model =
             , ( "cursor"
               , (case model.drag of
                     Just _ ->
-                        "grabbing"
+                        "-webkit-grabbing"
 
                     Nothing ->
-                        "grab"
+                        "-webkit-grab"
                 )
               )
             ]
@@ -110,21 +127,45 @@ gestureLayer model =
 layerStyles : String
 layerStyles =
     """
-polyline {
-    fill: none;
-    stroke: rgba(0,0,0,0.5);
-    stroke-width: 1px;
+.tile polyline {
+  fill: none;
+  stroke: #fff;
+  stroke-linejoin: round;
+  stroke-linecap: round;
 }
-polygon {
-    fill: rgba(0,0,0,0.2);
-    stroke: rgba(0,0,0,0.2);
-    stroke-width: 1px;
+.tile polygon {
+  fill: none;
+  stroke: #fff;
+  stroke-linejoin: round;
+  stroke-linecap: round;
 }
-circle {
-    fill: rgba(255,0,0,0.2);
-    stroke: rgba(255,0,0,0.2);
-    stroke-width: 1px;
+.tile circle {
+  fill: none;
+  stroke: #fff;
+  stroke-linejoin: round;
+  stroke-linecap: round;
 }
+.tile path {
+  fill: none;
+  stroke: #fff;
+  stroke-linejoin: round;
+  stroke-linecap: round;
+}
+
+.tile .water-layer, .tile .river, .tile .stream, .tile .canal { fill: none; stroke: #9DD9D2; stroke-width: 1.5px; }
+.tile .water, .tile .ocean { fill: #9DD9D2; }
+.tile .riverbank { fill: #9DD9D2; }
+.tile .water_boundary, .tile .ocean_boundary, .tile .riverbank_boundary { fill: none; stroke: #93cbc4; stroke-width: 0.5px; }
+.tile .major_road { stroke: #fb7b7a; stroke-width: 1px; }
+.tile .minor_road { stroke: #999; stroke-width: 0.5px; }
+.tile .highway { stroke: #FA4A48; stroke-width: 1.5px; }
+.tile .transit-layer { stroke: none; }
+.tile .buildings-layer { stroke: #987284; stroke-width: 0.15px; }
+.tile .urban_area { fill: #987284; stroke: #987284; stroke-width: 0.15px; }
+.tile .park, .tile .nature_reserve, .tile .wood, .tile .protected_land { fill: #88D18A; stroke: #88D18A; stroke-width: 0.5px; }
+.tile .pier { fill: #fff; stroke: #fff; stroke-width: 0.5px; }
+.tile .rail { stroke: #503D3F; stroke-width: 0.5px; }
+
 /*
 polyline {
     fill: none;
@@ -180,13 +221,13 @@ circle {
     """
 
 
-tileLayer : (Tile -> ( Tile, List ( String, GeoJson ) )) -> Transform -> Svg msg
+tileLayer : (Tile -> ( Tile, List Feature )) -> Transform -> Svg msg
 tileLayer tileToGeoJsonTile transform =
     LowLevel.tileLayer tileToGeoJsonTile (tile transform) transform
 
 
-tile : Transform -> ( Tile, List ( String, GeoJson ) ) -> Svg msg
-tile transform ( { z, x, y }, geojsonGroups ) =
+tile : Transform -> ( Tile, List Feature ) -> Svg msg
+tile transform ( { z, x, y }, featureList ) =
     let
         ( _, scale, _, _ ) =
             LowLevel.toTransformScaleCoverCenter transform
@@ -209,10 +250,66 @@ tile transform ( { z, x, y }, geojsonGroups ) =
                    )
     in
         Svg.g []
-            (geojsonGroups
-                |> List.filterMap isInteresting
-                |> List.map (SimpleGeoJson.renderGeoJson project)
+            (featureList
+                --|> List.filterMap isInteresting
+                |> List.filter
+                    (\{ properties } ->
+                        case properties of
+                            Nothing ->
+                                False
+
+                            Just props ->
+                                not props.labelPlacement
+                                    && (props.minZoom < transform.zoom)
+                    )
+                |> List.sortBy
+                    (\{ properties } ->
+                        case properties of
+                            Nothing ->
+                                0
+
+                            Just props ->
+                                props.sortRank
+                    )
+                |> List.concatMap (renderFeature project)
             )
+
+
+renderFeature : (GeoJson.Position -> Point) -> Feature -> List (Svg msg)
+renderFeature project { properties, geometry } =
+    let
+        attributes =
+            Maybe.map
+                (\p ->
+                    [ Svg.Attributes.class (p.layerName ++ "-layer " ++ p.kind) ]
+                )
+                properties
+                |> Maybe.withDefault []
+
+        children =
+            case geometry of
+                GeoJson.Point position ->
+                    SimpleGeoJson.renderGeoJsonPoint project attributes position
+
+                GeoJson.MultiPoint positionList ->
+                    List.concatMap (SimpleGeoJson.renderGeoJsonPoint project attributes) positionList
+
+                GeoJson.LineString positionList ->
+                    SimpleGeoJson.renderGeoJsonLineString project attributes positionList
+
+                GeoJson.MultiLineString positionListList ->
+                    List.concatMap (SimpleGeoJson.renderGeoJsonLineString project attributes) positionListList
+
+                GeoJson.Polygon positionListList ->
+                    SimpleGeoJson.renderGeoJsonPolygon project attributes positionListList
+
+                GeoJson.MultiPolygon positionListListList ->
+                    List.concatMap (SimpleGeoJson.renderGeoJsonPolygon project attributes) positionListListList
+
+                GeoJson.GeometryCollection geometryList ->
+                    List.concatMap (SimpleGeoJson.renderGeoJsonGeometry project attributes) geometryList
+    in
+        children
 
 
 isInteresting : ( String, GeoJson ) -> Maybe GeoJson
@@ -223,11 +320,80 @@ isInteresting ( groupName, geojson ) =
         Nothing
 
 
+toFeatures : List ( String, GeoJson ) -> List Feature
+toFeatures =
+    List.concatMap (uncurry namedGeoJsonToFeatures)
+
+
+namedGeoJsonToFeatures : String -> GeoJson -> List Feature
+namedGeoJsonToFeatures layerName ( geoJsonObject, _ ) =
+    namedGeoJsonObjectToFeatures layerName geoJsonObject
+
+
+namedGeoJsonObjectToFeatures : String -> GeoJson.GeoJsonObject -> List Feature
+namedGeoJsonObjectToFeatures layerName geoJsonObject =
+    case geoJsonObject of
+        GeoJson.Geometry geometry ->
+            [ { properties = Nothing, geometry = geometry } ]
+
+        GeoJson.Feature featureObject ->
+            [ namedGeoJsonFeatureObjectToFeature layerName featureObject ]
+
+        GeoJson.FeatureCollection featureCollection ->
+            List.map (namedGeoJsonFeatureObjectToFeature layerName) featureCollection
+
+
+namedGeoJsonFeatureObjectToFeature : String -> GeoJson.FeatureObject -> Feature
+namedGeoJsonFeatureObjectToFeature layerName { properties, geometry } =
+    { properties = decodeProperties layerName properties
+    , geometry =
+        geometry
+            |> Maybe.withDefault (GeoJson.GeometryCollection [])
+    }
+
+
+decodeProperties : String -> Decode.Value -> Maybe FeatureProperties
+decodeProperties layerName properties =
+    Decode.decodeValue (propertiesDecoder layerName) properties
+        |> Result.toMaybe
+
+
+propertiesDecoder : String -> Decoder FeatureProperties
+propertiesDecoder layerName =
+    Decode.succeed (FeatureProperties layerName)
+        & Decode.maybe (Decode.field "name" Decode.string)
+        & Decode.field "kind" Decode.string
+        & Decode.field "min_zoom" Decode.float
+        & Decode.field "sort_rank" Decode.int
+        & (Decode.maybe (Decode.field "label_placement" Decode.bool)
+            |> Decode.map
+                (\placement ->
+                    case placement of
+                        Just bool ->
+                            bool
+
+                        Nothing ->
+                            False
+                )
+          )
+
+
+(&) : Decode.Decoder (a -> b) -> Decode.Decoder a -> Decode.Decoder b
+(&) =
+    Decode.map2 (<|)
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         GeoJsonTileResponse key data ->
-            { model | tiles = Dict.insert key data model.tiles } ! []
+            { model
+                | tiles =
+                    Dict.insert key
+                        (RemoteData.map toFeatures data)
+                        model.tiles
+            }
+                ! []
 
         ZoomIn ->
             let
@@ -413,7 +579,7 @@ vectorTileDecoder =
     Decode.keyValuePairs GeoJson.decoder
 
 
-tileToGeoJson : Model -> Tile -> ( Tile, List ( String, GeoJson ) )
+tileToGeoJson : Model -> Tile -> ( Tile, List Feature )
 tileToGeoJson model tile =
     let
         comparable =
