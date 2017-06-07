1a05d096a8a63fbdc0c2ff48a6357f7819215a4a
Jonas Coch <jonas@iosphere.de>
2017-06-07 20:26:38 +0200

    Use Svg in zoom buttons

--- src/SlippyMap/Control/Zoom.elm
+++ src/SlippyMap/Control/Zoom.elm
@@ -8,6 +8,8 @@ import SlippyMap.Map.Msg as Msg exposing (Msg(ZoomIn, ZoomOut))
 import Html exposing (Html)
 import Html.Attributes
 import Html.Events
+import Svg exposing (Svg)
+import Svg.Attributes
 
 
 control : Layer.RenderState -> Html Msg
@@ -21,13 +23,60 @@ control renderState =
             ]
         ]
         [ Html.button
-            [ Html.Attributes.style [ ( "cursor", "pointer" ) ]
+            [ Html.Attributes.style
+                (buttonStyleProperties
+                    ++ [ ( "top", "12px" )
+                       , ( "border-radius", "2px 2px 0 0" )
+                       ]
+                )
             , Html.Events.onClick ZoomIn
             ]
-            [ Html.text "+" ]
+            [ Svg.svg
+                [ Svg.Attributes.width "24"
+                , Svg.Attributes.height "24"
+                ]
+                [ Svg.path
+                    [ Svg.Attributes.d "M6,12L18,12M12,6L12,18"
+                    , Svg.Attributes.strokeWidth "2"
+                    , Svg.Attributes.stroke "#444"
+                    ]
+                    []
+                ]
+            ]
         , Html.button
-            [ Html.Attributes.style [ ( "cursor", "pointer" ) ]
+            [ Html.Attributes.style
+                (buttonStyleProperties
+                    ++ [ ( "top", "37px" )
+                       , ( "border-radius", "0 0 2px 2px" )
+                       ]
+                )
             , Html.Events.onClick ZoomOut
             ]
-            [ Html.text "-" ]
+            [ Svg.svg
+                [ Svg.Attributes.width "24"
+                , Svg.Attributes.height "24"
+                ]
+                [ Svg.path
+                    [ Svg.Attributes.d "M6,12L18,12"
+                    , Svg.Attributes.strokeWidth "2"
+                    , Svg.Attributes.stroke "#444"
+                    ]
+                    []
+                ]
+            ]
         ]
+
+
+buttonStyleProperties : List ( String, String )
+buttonStyleProperties =
+    [ ( "cursor", "pointer" )
+    , ( "box-sizing", "content-box" )
+    , ( "width", "24px" )
+    , ( "height", "24px" )
+    , ( "padding", "0" )
+    , ( "background-color", "#fff" )
+    , ( "color", "#000" )
+    , ( "border", "1px solid #aaa" )
+    , ( "position", "absolute" )
+    , ( "left", "12px" )
+    ]
