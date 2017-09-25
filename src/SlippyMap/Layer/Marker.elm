787da12efe63c3880b11c96122a7612ae6be9401
Jonas Coch <jonas@iosphere.de>
2017-09-25 22:22:59 +0200

    Add basic event setup to base marker layer

--- src/SlippyMap/Layer/Marker.elm
+++ src/SlippyMap/Layer/Marker.elm
@@ -11,11 +11,15 @@ module SlippyMap.Layer.Marker
 
 -}
 
+import Json.Decode exposing (Decoder)
 import SlippyMap.Geo.Location as Location exposing (Location)
 import SlippyMap.Layer as Layer exposing (Layer)
+import SlippyMap.Map.Events as Events exposing (Event)
 import SlippyMap.Map.Map as Map exposing (Map)
 import Svg exposing (Svg)
 import Svg.Attributes
+import Svg.Events
+import VirtualDom
 
 
 -- CONFIG
@@ -27,16 +31,30 @@ type Config marker msg
     = Config
         { location : marker -> Location
         , icon : marker -> Svg msg
+        , toEvents : List (marker -> Event marker msg)
         }
 
 
 {-| -}
-config : (marker -> Location) -> (marker -> Svg msg) -> Config marker msg
-config toLocation toIcon =
+config : (marker -> Location) -> (marker -> Svg msg) -> List (marker -> Event marker msg) -> Config marker msg
+config toLocation toIcon toEvents =
     Config
         { location = toLocation
         , icon = toIcon
+        , toEvents = toEvents
         }
+
+
+
+-- {-| TODO: Is this needed?
+-- -}
+-- on : String -> (marker -> Decoder msg) -> Config marker msg -> Config marker msg
+-- on name toDecoder (Config ({ events } as config)) =
+--     Config
+--         { config
+--             | events =
+--                 Events.onn name toDecoder :: events
+--         }
 
 
 {-| -}
@@ -48,22 +66,14 @@ layer config markers =
 
 {-| -}
 render : Config marker msg -> List marker -> Map msg -> Svg msg
-render ((Config { location }) as config) markers renderParameters =
+render ((Config { location }) as config) markers map =
     let
         locatedMarkers =
             List.map
                 (\marker -> ( location marker, marker ))
                 markers
 
-        -- centerPoint =
-        --     renderState.centerPoint
-        -- bounds =
-        --     renderState.locationBounds
         locatedMarkersFiltered =
-            -- List.filter
-            --     (\( location, _ ) ->
-            --         Location.isInsideBounds bounds location
-            --     )
             locatedMarkers
     in
     Svg.svg
@@ -76,7 +86,7 @@ render ((Config { location }) as config) markers renderParameters =
         , Svg.Attributes.height "100%"
         , Svg.Attributes.style "position: absolute;"
         ]
-        (List.map (marker config renderParameters) locatedMarkersFiltered)
+        (List.map (marker config map) locatedMarkersFiltered)
 
 
 marker : Config marker msg -> Map msg -> ( Location, marker ) -> Svg msg
@@ -84,14 +94,30 @@ marker (Config config) map ( location, marker ) =
     let
         markerPoint =
             Map.locationToScreenPoint map location
+
+        events =
+            List.map
+                (\toEvent ->
+                    let
+                        { name, toDecoder } =
+                            toEvent marker
+                    in
+                    VirtualDom.onWithOptions name
+                        { stopPropagation = True
+                        , preventDefault = False
+                        }
+                        (toDecoder marker)
+                )
+                config.toEvents
     in
     Svg.g
-        [ Svg.Attributes.transform
+        (Svg.Attributes.transform
             ("translate("
                 ++ toString markerPoint.x
                 ++ " "
                 ++ toString markerPoint.y
                 ++ ")"
             )
-        ]
+            :: (Svg.Attributes.pointerEvents "auto" :: events)
+        )
         [ config.icon marker ]
